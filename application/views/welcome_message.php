27cef588989afad4768d037632c6af5ead2fadb6
Gran Sabandal <ggsabandal@usc.edu.ph>
2021-10-07 15:45:53 +0800

    R-02 Fix: Adjust Code Format
    Code format for welcome_home.php and profile.php.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,163 +1,155 @@
-<!-- start body content here -->
-
-<section id="dashboard-ecommerce">
-                    <div class="row match-height">
-                        <!-- Medal Card -->
-                        <div class="col-xl-4 col-md-6 col-12">
-                            <div class="card card-congratulation-medal">
-                                <div class="card-body">
-                                    <h5>Welcome!</h5>
-                                    <p class="card-text font-small-3">You currently have:</p>
-                                    <h3 class="mb-75 pt-50">
-                                        3 Submissions
-                                    </h3>
-                                    <a href="#" type="button" class="btn btn-success">View My Submissions</a>
-                                  
-                                </div>
+<div class="app-content content ">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper container-xxl p-0">
+        <div class="content-header row"></div>
+        <div class="content-body">
+            <!-- start body content here -->
+            <section id="dashboard-ecommerce">
+                <div class="row match-height">
+                    <!-- Medal Card -->
+                    <div class="col-xl-4 col-md-6 col-12">
+                        <div class="card card-congratulation-medal">
+                            <div class="card-body">
+                                <h5>Welcome!</h5>
+                                <p class="card-text font-small-3">You currently have:</p>
+                                <h3 class="mb-75 pt-50">3 Submissions</h3>
+                                <a href="#" type="button" class="btn btn-success">View My Submissions</a>
                             </div>
                         </div>
-                        <!--/ Medal Card -->
-
-                        <!-- Statistics Card -->
-                        <div class="col-xl-8 col-md-6 col-12">
-                            <div class="card card-statistics">
-                                <div class="card-header">
-                                    <h4 class="card-title">Submissions Progress</h4>
-                                    <div class="d-flex align-items-center">
-                                       
-                                    </div>
-                                </div>
-                                <div class="card-body statistics-body">
-                                    <div class="row">
-                                    
-                                      
-                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
-                                            <div class="media">
-                                                <div class="avatar bg-light-danger mr-2">
-                                                    <div class="avatar-content">
-                                                        <i data-feather='activity'></i>
-                                                    </div>
-                                                </div>
-                                                <div class="media-body my-auto">
-                                                    <h4 class="font-weight-bolder mb-0">1</h4>
-                                                    <p class="card-text font-small-3 mb-0">Pending</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
-                                            <div class="media">
-                                                <div class="avatar bg-light-info mr-2">
-                                                    <div class="avatar-content">
-                                                        <i data-feather='eye'></i>
-                                                    </div>
-                                                </div>
-                                                <div class="media-body my-auto">
-                                                    <h4 class="font-weight-bolder mb-0">1</h4>
-                                                    <p class="card-text font-small-3 mb-0">Under Review</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-3 col-sm-6 col-12">
-                                            <div class="media">
-                                                <div class="avatar bg-light-success mr-2">
-                                                    <div class="avatar-content">
-                                                        <i data-feather='user-check'></i>
-                                                    </div>
-                                                </div>
-                                                <div class="media-body my-auto">
-                                                    <h4 class="font-weight-bolder mb-0">1</h4>
-                                                    <p class="card-text font-small-3 mb-0">Completed</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!--/ Statistics Card -->
                     </div>
-                    <?php 
-                        if($_SESSION['level']>1){
-                     ?>
-                    <div class="row match-height">
-                        <!-- Medal Card -->
-                        <div class="col-xl-4 col-md-6 col-12">
-                            <div class="card card-congratulation-medal">
-                                <div class="card-body">
-                            
-                                    <p class="card-text font-small-3">You currently have:</p>
-                                    <h3 class="mb-75 pt-50">
-                                        10 Items to Review
-                                    </h3>
-                                    <a href="#" type="button" class="btn btn-success">View Review Items</a>
-                                  
-                                </div>
-                            </div>
-                        </div>
-                        <!--/ Medal Card -->
+                    <!--/ Medal Card -->
 
-                        <!-- Statistics Card -->
-                        <div class="col-xl-8 col-md-6 col-12">
-                            <div class="card card-statistics">
-                                <div class="card-header">
-                                    <h4 class="card-title">Review Items</h4>
-                                    <div class="d-flex align-items-center">
-                                       
+                    <!-- Statistics Card -->
+                    <div class="col-xl-8 col-md-6 col-12">
+                        <div class="card card-statistics">
+                            <div class="card-header">
+                                <h4 class="card-title">Submissions Progress</h4>
+                                <div class="d-flex align-items-center"></div>
+                            </div>
+                            <div class="card-body statistics-body">
+                                <div class="row">
+                                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
+                                        <div class="media">
+                                            <div class="avatar bg-light-danger mr-2">
+                                                <div class="avatar-content">
+                                                    <i data-feather='activity'></i>
+                                                </div>
+                                            </div>
+                                            <div class="media-body my-auto">
+                                                <h4 class="font-weight-bolder mb-0">1</h4>
+                                                <p class="card-text font-small-3 mb-0">Pending</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="card-body statistics-body">
-                                    <div class="row">
-                                    
-                                      
-                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
-                                            <div class="media">
-                                                <div class="avatar bg-light-danger mr-2">
-                                                    <div class="avatar-content">
-                                                        <i data-feather='activity'></i>
-                                                    </div>
-                                                </div>
-                                                <div class="media-body my-auto">
-                                                    <h4 class="font-weight-bolder mb-0">8</h4>
-                                                    <p class="card-text font-small-3 mb-0">Pending</p>
+                                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                                        <div class="media">
+                                            <div class="avatar bg-light-info mr-2">
+                                                <div class="avatar-content">
+                                                    <i data-feather='eye'></i>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
-                                            <div class="media">
-                                                <div class="avatar bg-light-info mr-2">
-                                                    <div class="avatar-content">
-                                                        <i data-feather='eye'></i>
-                                                    </div>
-                                                </div>
-                                                <div class="media-body my-auto">
-                                                    <h4 class="font-weight-bolder mb-0">1</h4>
-                                                    <p class="card-text font-small-3 mb-0">Under Review</p>
-                                                </div>
+                                            <div class="media-body my-auto">
+                                                <h4 class="font-weight-bolder mb-0">1</h4>
+                                                <p class="card-text font-small-3 mb-0">Under Review</p>
                                             </div>
                                         </div>
-                                        <div class="col-xl-3 col-sm-6 col-12">
-                                            <div class="media">
-                                                <div class="avatar bg-light-success mr-2">
-                                                    <div class="avatar-content">
-                                                        <i data-feather='user-check'></i>
-                                                    </div>
+                                    </div>
+                                    <div class="col-xl-3 col-sm-6 col-12">
+                                        <div class="media">
+                                            <div class="avatar bg-light-success mr-2">
+                                                <div class="avatar-content">
+                                                    <i data-feather='user-check'></i>
                                                 </div>
-                                                <div class="media-body my-auto">
-                                                    <h4 class="font-weight-bolder mb-0">1</h4>
-                                                    <p class="card-text font-small-3 mb-0">Completed</p>
-                                                </div>
+                                            </div>
+                                            <div class="media-body my-auto">
+                                                <h4 class="font-weight-bolder mb-0">1</h4>
+                                                <p class="card-text font-small-3 mb-0">Completed</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!--/ Statistics Card -->
                     </div>
+                    <!--/ Statistics Card -->
+                </div>
+
+                <?php 
+                    if($_SESSION['level']>1){
+                ?>
+                <div class="row match-height">
+                    <!-- Medal Card -->
+                    <div class="col-xl-4 col-md-6 col-12">
+                        <div class="card card-congratulation-medal">
+                            <div class="card-body">
+                                <p class="card-text font-small-3">You currently have:</p>
+                                <h3 class="mb-75 pt-50">10 Items to Review</h3>
+                                <a href="#" type="button" class="btn btn-success">View Review Items</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/ Medal Card -->
+
+                    <!-- Statistics Card -->
+                    <div class="col-xl-8 col-md-6 col-12">
+                        <div class="card card-statistics">
+                            <div class="card-header">
+                                <h4 class="card-title">Review Items</h4>
+                                <div class="d-flex align-items-center"></div>
+                            </div>
+                            <div class="card-body statistics-body">
+                                <div class="row">
+                                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
+                                        <div class="media">
+                                            <div class="avatar bg-light-danger mr-2">
+                                                <div class="avatar-content">
+                                                    <i data-feather='activity'></i>
+                                                </div>
+                                            </div>
+                                            <div class="media-body my-auto">
+                                                <h4 class="font-weight-bolder mb-0">8</h4>
+                                                <p class="card-text font-small-3 mb-0">Pending</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                                        <div class="media">
+                                            <div class="avatar bg-light-info mr-2">
+                                                <div class="avatar-content">
+                                                    <i data-feather='eye'></i>
+                                                </div>
+                                            </div>
+                                            <div class="media-body my-auto">
+                                                <h4 class="font-weight-bolder mb-0">1</h4>
+                                                <p class="card-text font-small-3 mb-0">Under Review</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-3 col-sm-6 col-12">
+                                        <div class="media">
+                                            <div class="avatar bg-light-success mr-2">
+                                                <div class="avatar-content">
+                                                    <i data-feather='user-check'></i>
+                                                </div>
+                                            </div>
+                                            <div class="media-body my-auto">
+                                                <h4 class="font-weight-bolder mb-0">1</h4>
+                                                <p class="card-text font-small-3 mb-0">Completed</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        <!--/ Statistics Card -->
                     <?php
                         }
                     ?>
-
-</section>
-
-<!-- end body content here -->
+                </div>
+            </section>
+            <!-- end body content here -->
+        </div>
+    </div>
+</div>
