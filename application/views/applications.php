e87490a336b4536448e6d16cccb0839040e96036
geekaholick <33748869+geekaholick@users.noreply.github.com>
2021-10-08 08:28:41 +0800

    cvm-01 Feat: Create application module

---
+++ application/views/applications.php
@@ -0,0 +1,74 @@
+<!-- start body content here -->
+
+<section id="dashboard-ecommerce">
+                    <div class="row match-height">
+                        <!-- Medal Card -->
+                        <div class="col-lg-12">
+                            <div class="card card-congratulation-medal">
+                                <div class="card-body">
+                                    <h5>Applications</h5>
+                                    <p class="card-text font-small-3">Show Entries</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/ Medal Card -->
+
+                        
+                    </div>
+                    <div class="row match-height">
+                        <!-- Medal Card -->
+                        <div class="col-lg-12"  style="font-size: 16px;">
+                            
+                                        <table class="table table-striped">
+                                            <thead>
+                                                <tr width="15%">
+                                                    <th scope="col">#</th>
+                                                    <th scope="col">Client</th>
+                                                    <th scope="col">Project Title</th>
+                                                    <th scope="col">Date Submitted</th>
+                                                    <th scope="col">Status</th>
+                                                    <th scope="col">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">1</th>
+                                                    <td>Glenn Pepito</td>
+                                                    <td>Research on Matter</td>
+                                                    <td>8/4/2021</td>
+                                                    <td>PENDING</td>
+                                                    <td><a href="Applications/application_details">Show</a></td>
+
+                                                    
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">2</th>
+                                                    <td>Gran Sabandal</td>
+                                                    <td>Research on does not Matter</td>
+                                                    <td>8/4/2021</td>
+                                                    <td>LACKING</td>                                                    
+                                                    <td><a href="Applications/application_details.php">Show</a></td>                                                    
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">3</th>
+                                                    <td>Angie Ceniza</td>
+                                                    <td>Research on the Matter of Fact</td>
+                                                    <td>APPROVED</td>                                                    
+                                                    <td>8/4/2021</td>
+                                                    <td><a href="Applications/application_details">Show</a></td>
+                                                    
+                                                </tr>
+                                               
+                                            </tbody>
+                                        </table>
+                                  
+                            
+                        </div>
+                        <!--/ Medal Card -->
+
+                        
+                    </div>
+
+</section>
+
+<!-- end body content here -->
