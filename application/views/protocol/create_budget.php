1121b1168dd1e39f6345fdac02481155e6f08c9c
njompad <njompad@gmail.com>
2021-11-11 16:54:30 +0800

    feat(Create Budget Module): updated UI

--- application/views/protocol/create_budget.php
+++ application/views/protocol/create_budget.php
@@ -11,35 +11,37 @@
                             <div class="card-header">
                                 <h4 class="card-title">Section 1: Personnel Services</h4>
                             </div>
+                            <?php
+                                foreach ($section1 as $particular): 
+                            ?>
                             <div class="card-body">
+                                <div class="row d-flex align-items-end">
+                                    <span class="col-md-4 col-12">Particulars</span>
+                                    <span class="col-md-2 col-12">Personal</span>
+                                    <span class="col-md-2 col-12">External Source</span>
+                                    <span class="col-md-2 col-12">Interval Source</span>
+                                </div>
                                 <div class="repeater-default">
                                     <div data-repeater-list="budegt1">
-                                        <?php
-                                            foreach ($section1 as $particular): 
-                                        ?>
                                         <div data-repeater-item>
                                             <div class="row d-flex align-items-end">
                                                 <div class="col-md-4 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemname">Particulars</label>
                                                         <input type="text" class="form-control" id="itemname" aria-describedby="itemname" placeholder="" value="<?=$particular->particular;?>"/>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemcost">Personal</label>
                                                         <input type="number" class="form-control" id="itemcost" aria-describedby="itemcost" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">External Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">Interval Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
@@ -54,7 +56,6 @@
                                             </div>
                                             <hr />
                                         </div>
-                                        <?php endforeach;?>
                                     </div>
                                     <div class="row">
                                         <div class="col-12">
@@ -66,6 +67,7 @@
                                     </div>
                                 </div>
                             </div>
+                            <?php endforeach;?>
                         </div>
                     </div>
                 </div>
@@ -75,35 +77,37 @@
                             <div class="card-header">
                                 <h4 class="card-title">Section 2: Maintenance and Other Operating Expenses (MOOE)</h4>
                             </div>
+                            <?php
+                                foreach ($section2 as $particular): 
+                            ?>
                             <div class="card-body">
+                                <div class="row d-flex align-items-end">
+                                    <span class="col-md-4 col-12">Particulars</span>
+                                    <span class="col-md-2 col-12">Personal</span>
+                                    <span class="col-md-2 col-12">External Source</span>
+                                    <span class="col-md-2 col-12">Interval Source</span>
+                                </div>
                                 <div class="repeater-default">
                                     <div data-repeater-list="budegt2">
-                                        <?php
-                                            foreach ($section2 as $particular): 
-                                        ?>
                                         <div data-repeater-item>
                                             <div class="row d-flex align-items-end">
                                                 <div class="col-md-4 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemname">Particulars</label>
                                                         <input type="text" class="form-control" id="itemname" aria-describedby="itemname" placeholder="" value="<?=$particular->particular;?>"/>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemcost">Personal</label>
                                                         <input type="number" class="form-control" id="itemcost" aria-describedby="itemcost" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">External Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">Interbal Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
@@ -118,7 +122,6 @@
                                             </div>
                                             <hr />
                                         </div>
-                                        <?php endforeach;?>
                                     </div>
                                     <div class="row">
                                         <div class="col-12">
@@ -130,6 +133,7 @@
                                     </div>
                                 </div>
                             </div>
+                            <?php endforeach;?>
                         </div>
                     </div>
                 </div>
@@ -140,31 +144,33 @@
                                 <h4 class="card-title">Section 3: Capital Outlay</h4>
                             </div>
                             <div class="card-body">
+                                <div class="row d-flex align-items-end">
+                                    <span class="col-md-4 col-12">Particulars</span>
+                                    <span class="col-md-2 col-12">Personal</span>
+                                    <span class="col-md-2 col-12">External Source</span>
+                                    <span class="col-md-2 col-12">Interval Source</span>
+                                </div>
                                 <div class="repeater-default">
                                     <div data-repeater-list="budegt3">
                                         <div data-repeater-item>
                                             <div class="row d-flex align-items-end">
                                                 <div class="col-md-4 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemname">Particulars</label>
                                                         <input type="text" class="form-control" id="itemname" aria-describedby="itemname" placeholder="" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemcost">Personal</label>
                                                         <input type="number" class="form-control" id="itemcost" aria-describedby="itemcost" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">External Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">Interbal Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
@@ -200,31 +206,33 @@
                                 <h4 class="card-title">Section 4: Administrative Cost</h4>
                             </div>
                             <div class="card-body">
+                                <div class="row d-flex align-items-end">
+                                    <span class="col-md-4 col-12">Particulars</span>
+                                    <span class="col-md-2 col-12">Personal</span>
+                                    <span class="col-md-2 col-12">External Source</span>
+                                    <span class="col-md-2 col-12">Interval Source</span>
+                                </div>
                                 <div class="repeater-default">
                                     <div data-repeater-list="budegt4">
                                         <div data-repeater-item>
                                             <div class="row d-flex align-items-end">
                                                 <div class="col-md-4 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemname">Particulars</label>
                                                         <input type="text" class="form-control" id="itemname" aria-describedby="itemname" placeholder="" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemcost">Personal</label>
                                                         <input type="number" class="form-control" id="itemcost" aria-describedby="itemcost" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">External Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-2 col-12">
                                                     <div class="form-group">
-                                                        <label for="itemquantity">Interbal Source</label>
                                                         <input type="number" class="form-control" id="itemquantity" aria-describedby="itemquantity" placeholder="0" />
                                                     </div>
                                                 </div>
